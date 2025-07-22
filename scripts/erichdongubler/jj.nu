erichdongubler-dotfiles/nushell

commit 6f16218d1a140791f9a18efee41a1426f4360c1e

style(jj): s/-T/--template

scripts/erichdongubler/jj.nu
@@ -31,7 +31,7 @@ export def "bookmark resolve" [
       self.added_targets().filter(|t| !t.hidden()).map(|t| separate(" ", self.name(), t.commit_id().short()) ++ "\n")
     )
   '
-  let bookmarks_with_single_visible_added_target = jj bookmark list --conflicted -T $template | parse '{bookmark} {commit}'
+  let bookmarks_with_single_visible_added_target = jj bookmark list --conflicted --template $template | parse '{bookmark} {commit}'
   for entry in $bookmarks_with_single_visible_added_target {
     jj bookmark set $entry.bookmark --revision $entry.commit
   }
@@ -59,7 +59,7 @@ export def "gh pr push" [
   git push --force $'git@github.com:($owner)/($repo).git' $'($local_branch_rev):($branch_name)'
 }
 
-export def "nu-complete jj bookmark list" [] {
+def "nu-complete jj bookmark list" [] {
   jj bookmark list --quiet --template 'name ++ "\n"' | lines | uniq
 }
 

commit 057e45fdb716b56a54c83a2dad552a0f17886239

fix(jj): restore `CTRL + G` binding for version refs
Broke this in 6f16218d1a140791f9a18efee41a1426f4360c1e, because I'd not split it out and tested it by accident.

scripts/erichdongubler/jj.nu
@@ -80,7 +80,8 @@ export def "gh pr push" [
   run-external $bin ...$args
 }
 
-def "nu-complete jj bookmark list" [] {
+# NOTE: This is `export`ed for the sake of `CTRL + G` completion.
+export def "nu-complete jj bookmark list" [] {
   jj bookmark list --quiet --template 'name ++ "\n"' | lines | uniq
 }
 